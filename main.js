04d5e9a7ca46fe3d7fe8a93f8081699a8022b3fa
lucybrewton1 <76742499+lucybrewton1@users.noreply.github.com>
2022-01-06 10:34:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function savePhoto() {
+    save("projectFilter.png");
+}
+function preload() {
+
+}
+function setup() {
+    canvas=createCanvas(600,500);
+    canvas.position(660,200);
+}
+function draw() {
+
+}
